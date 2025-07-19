3af83bb149821fcf1cbdc206b4a764ed4ba83400
rohwang <eight6717@naver.com>
2025-07-19 21:03:22 +0900

    alpha 0.1.1 Spawner Update
    Now, walls and obstacles will damage your stamina.
    
    Now jumping will consume even more stamina.
    
    Now you can pass through walls and obstacles, but that doesn’t mean you can avoid damage.
    
    Now spawners will now spawn obstacles only when they are visible to the camera.

--- Assets/Scripts/FlyTest/Object/CollideObstacle.cs
+++ Assets/Scripts/FlyTest/Object/CollideObstacle.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class CollideObstacle : MonoBehaviour
 {
-    [SerializeField]
-    Collider2D Player;
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("플레이어가 Obstacle에 충돌했습니다.");
-        }
-    }
+    public ObstacleSO obstacleData;
 
 }
