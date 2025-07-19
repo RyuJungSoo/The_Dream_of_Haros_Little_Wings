df71dd4aa0f661ccd43d576a9f819810fd04ebd8
rohwang <eight6717@naver.com>
2025-07-19 21:55:41 +0900

    alpha 0.1.3 - Item Update
    Add item dropdown, but it will not function

--- Assets/Scripts/FlyTest/Object/CollideItem.cs
+++ Assets/Scripts/FlyTest/Object/CollideItem.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+    public enum Name
+    {
+        Wind,
+        Bell,
+        acorn
+    }
+
 public class CollideItem : MonoBehaviour
 {
     [SerializeField]
     Collider2D Player;
+
+    public Name state;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject.CompareTag("Player"))
         {
             Debug.Log("플레이어가 Item을 획득했습니다.");
+            if (state == Name.acorn)
+            {
+
+            }
+            if (state == Name.Bell)
+            {
+
+            }
+            if (state==Name.Wind)
+            {
+                
+            }
         }
     }
 
