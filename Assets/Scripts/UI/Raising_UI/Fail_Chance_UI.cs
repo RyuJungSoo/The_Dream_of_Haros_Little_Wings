72a8bbc26641dc7cc1a27f5c842b7375159db022
RyuJungSoo <goldfox10@naver.com>
2025-07-21 13:17:23 +0900

    Change Raising Stage UI

---
+++ Assets/Scripts/UI/Raising_UI/Fail_Chance_UI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Fail_Chance_UI : MonoBehaviour
+{
+    // ¿Œµ¶Ω∫ 1 : ª°∞≠
+    // ¿Œµ¶Ω∫ 2 : ¡÷»≤
+    // ¿Œµ¶Ω∫ 3 : ≥Î∂˚
+    // ¿Œµ¶Ω∫ 4 : √ ∑œ
+    // ¿Œµ¶Ω∫ 5 : ∆ƒ∂˚
+    string[] Colors = { "#C0504D", "#FFBA00", "#EED050", "#9BBB59", "#4F81BD"};
+
+    public void UIUpdate(int Fail_Value)
+    {
+        TextMeshProUGUI textMeshProUGUI = GetComponentInChildren<TextMeshProUGUI>();
+        textMeshProUGUI.text = "Ω«∆–¿≤ " + Fail_Value.ToString() + "%";
+
+        if (Fail_Value > 80)
+            GetComponent<Image>().color = GetColor(0);
+        else if (Fail_Value > 60)
+            GetComponent<Image>().color = GetColor(1);
+        else if (Fail_Value > 40)
+            GetComponent<Image>().color = GetColor(2);
+        else if (Fail_Value > 20)
+            GetComponent<Image>().color = GetColor(3);
+        else
+            GetComponent<Image>().color = GetColor(4);
+    }
+
+    private Color GetColor(int index)
+    {
+        Color color;
+        ColorUtility.TryParseHtmlString(Colors[index], out color);
+        return color;
+    }
+
+}
